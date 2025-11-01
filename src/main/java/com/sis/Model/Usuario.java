9cabe4d2f8064b8675478c6c0587a8104d7c732b
GomezJuangl <gomezlandinezjuanguillermo@gmail.com>
2025-11-01 11:50:49 -0500

    FEAT: Implementation Entidad Usuario closes #172

---
+++ src/main/java/com/sis/Model/Usuario.java
@@ -0,0 +1,117 @@
+package com.sis.Model;
+
+import com.sis.Model.Enum.TipoDoc;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+public class Usuario {
+    private UUID id;
+    private String username;
+    private String hash;
+    private String nombres;
+    private String apellidos;
+    private String email;
+    private TipoDoc tipoDocumento;
+    private String numeroDocumento;
+    private String direccion;
+    private boolean activo;
+    private LocalDateTime creadoEn;
+
+    public Usuario() {
+        this.id = UUID.randomUUID();
+        this.activo = true;
+        this.creadoEn = LocalDateTime.now();
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getHash() {
+        return hash;
+    }
+
+    public void setHash(String hash) {
+        this.hash = hash;
+    }
+
+    public String getNombres() {
+        return nombres;
+    }
+
+    public void setNombres(String nombres) {
+        this.nombres = nombres;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    public String getNombreCompleto() {
+        return nombres + " " + apellidos;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public TipoDoc getTipoDocumento() {
+        return tipoDocumento;
+    }
+
+    public void setTipoDocumento(TipoDoc tipoDocumento) {
+        this.tipoDocumento = tipoDocumento;
+    }
+
+    public String getNumeroDocumento() {
+        return numeroDocumento;
+    }
+
+    public void setNumeroDocumento(String numeroDocumento) {
+        this.numeroDocumento = numeroDocumento;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public boolean isActivo() {
+        return activo;
+    }
+
+    public void setActivo(boolean activo) {
+        this.activo = activo;
+    }
+
+    public LocalDateTime getCreadoEn() {
+        return creadoEn;
+    }
+
+    public void setCreadoEn(LocalDateTime creadoEn) {
+        this.creadoEn = creadoEn;
+    }
+}
