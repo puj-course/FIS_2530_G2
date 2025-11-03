a6e86e05d9a3ba6aea96e941fa5a3835cba24e8e
Juandacun <juandacun.lesmes@gmail.com>
2025-11-02 21:40:17 -0500

    FEAT: Implementación UsuarioRepo, Closes #187

--- src/main/java/com/sis/Repository/UsuarioRepo.java
+++ src/main/java/com/sis/Repository/UsuarioRepo.java
@@ -1,4 +1,16 @@
 package com.sis.Repository;
 
-public interface UsuarioRepo {
+import com.sis.Model.Usuario;
+import org.springframework.boot.autoconfigure.data.jpa.JpaRepositoriesAutoConfiguration;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+@Repository
+public interface UsuarioRepo extends JpaRepository<Usuario,UUID> {
+    Optional<Usuario> findById(UUID id);
+    Usuario save(Usuario usuario);
+    Optional<Usuario> findByDocumento(String tipoDocumento, String numeroDocumento);
 }
