890b9f785e59996fd7405aad7a8ac512adaad92f
Juandacun <juandacun.lesmes@gmail.com>
2025-11-02 22:16:59 -0500

    Update UsuarioRepo, removed unused import

--- src/main/java/com/sis/Repository/UsuarioRepo.java
+++ src/main/java/com/sis/Repository/UsuarioRepo.java
@@ -1,7 +1,6 @@
 package com.sis.Repository;
 
 import com.sis.Model.Usuario;
-import org.springframework.boot.autoconfigure.data.jpa.JpaRepositoriesAutoConfiguration;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -10,6 +9,7 @@ import java.util.UUID;
 
 @Repository
 public interface UsuarioRepo extends JpaRepository<Usuario,UUID> {
+    
     Optional<Usuario> findById(UUID id);
     Usuario save(Usuario usuario);
     Optional<Usuario> findByDocumento(String tipoDocumento, String numeroDocumento);
