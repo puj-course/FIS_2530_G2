513f00a69fc054783732727e7b8781031898cd58
Jonathan Martinez <83783422+jhony-hacku@users.noreply.github.com>
2025-11-03 23:12:22 -0500

    fix: testing database was fixed along pom.xml #166

--- src/main/java/com/sis/Controller/LoginController.java
+++ src/main/java/com/sis/Controller/LoginController.java
@@ -9,14 +9,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class LoginController {
     @GetMapping("/login")   //ESTABLECER URL Y METODO PARA EJECUTAR EL METODO (GET ES PARA MOSTRAR DATOS SEGUN HTTP
-    public String loginPage(Model model){
-        model.addAttribute("username","Juan");
-        model.addAttribute("password","1234");
+    public String loginPage(){
         return "login";
-    }
-    @GetMapping("/error")
-    public String usuarioIncorrecto(){
-        return "Usuario Incorrecto";
     }
 }
 
