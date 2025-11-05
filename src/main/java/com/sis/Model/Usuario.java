e4760d8619e15c6fbef419c7432c2bdc0b3a00d3
Mundolel <samuxdrbx@gmail.com>
2025-11-04 22:08:57 -0500

    FIX: Weird problem solved. References #199

--- src/main/java/com/sis/Model/Usuario.java
+++ src/main/java/com/sis/Model/Usuario.java
@@ -14,6 +14,7 @@ import java.util.UUID;
 @Inheritance(strategy = InheritanceType.JOINED)
 public class Usuario {
 
+    @jakarta.persistence.Id
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
     @Column(columnDefinition = "uuid")
